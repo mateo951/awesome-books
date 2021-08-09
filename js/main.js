36ae7f0a082d5c15b360c4676120d3d49e09b981
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-08-09 17:09:12 -0500

    Add remove book from books array

--- js/main.js
+++ js/main.js
@@ -9,23 +9,36 @@ form.addEventListener("click", (event) => {
   let titleValue = inputs[0].value;
   let authorValue = inputs[1].value;
   console.log(titleValue, authorValue);
-  // Create book object
+
+  createBook(titleValue, authorValue);
+  removeBook();
+});
+
+let createBook = function (titleValue, authorValue) {
   let newBook = new Book(titleValue, authorValue);
   booksArray.push(newBook);
-  console.log(booksArray);
   let divSection = document.createElement("div");
   divSection.innerHTML =
-    "<p>Title</p>" +
-    "<p>Author</p>" +
+    `<p>${newBook.title}</p>` +
+    `<p>${newBook.author}</p>` +
     `<button class = 'removeButton'>Remove</button>`;
   divSection.id = `${booksArray.length - 1}`;
   document.body.appendChild(divSection);
+}
+
+let removeBook = function () {
+  // Remove div section
   let buttonId = document.querySelectorAll(".removeButton");
-  console.log(buttonId);
   for (let i = 0; i < buttonId.length; i = i + 1) {
     buttonId[i].addEventListener("click", function () {
       let sectionId = document.getElementById(`${i}`);
-      sectionId.remove();
+      // console.log(newBook.title);
+      // Remove book from books
+
+      console.log(sectionId);
+
+      //booksArray.splice(i, 1);
+      //sectionId.remove();
     });
   }
-});
+}
