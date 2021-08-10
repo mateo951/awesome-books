03617fd449046b478f80e94eafe902a3ac1b4b53
Alex M <alexandermorales.n1@gmail.com>
2021-08-10 17:56:21 -0500

    add insert book Class

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const booksDisplaySection = document.querySelector('#booksDisplay');
-const form = document.querySelector('#submitBttn');
+const booksDisplaySection = document.querySelector("#booksDisplay");
+const form = document.querySelector("#submitBttn");
 
 const Book = class {
   constructor(title, author) {
@@ -22,10 +22,43 @@ const Books = class {
 
 const books = new Books();
 
+const Methods = class {
+  static createBook(bookData) {
+    const newBook = new Book(bookData.title, bookData.author);
+    books.booksData.push(newBook);
+    this.insertBookStructure(newBook, false);
+  }
 
+  static removeBook(div) {
+    const removeBttn = document
+      .getElementById(div.id)
+      .querySelector(".removeButton");
+    removeBttn.addEventListener("click", () => {
+      books.booksData.splice(div.id, 1); // eslint-disable-next-line no-use-before-define
+      CheckInput();
+      div.remove();
+    });
+  }
+
+  static insertBookStructure(book, initialDisplay) {
+    const divSection = document.createElement("div");
+    divSection.innerHTML =
+      `<p>${book.title}</p>` +
+      `<p>${book.author}</p>` +
+      "<button class = 'removeButton'>Remove</button><br>";
+    if (!initialDisplay) {
+      divSection.id = `${books.booksData.length - 1}`;
+    } else {
+      divSection.id = `${Books.ids}`;
+      Books.ids += 1;
+    }
+    booksDisplaySection.appendChild(divSection);
+    this.removeBook(divSection);
+  }
+};
 
 function localStorageAv() {
-  const test = 'test';
+  const test = "test";
   try {
     localStorage.setItem(test, test);
     localStorage.removeItem(test);
@@ -37,7 +70,7 @@ function localStorageAv() {
 
 function HandleInputData() {
   const jsonData = JSON.stringify(books.booksData);
-  localStorage.setItem('data', jsonData);
+  localStorage.setItem("data", jsonData);
 }
 
 const storageAvailability = localStorageAv();
@@ -49,13 +82,13 @@ const CheckInput = () => {
 };
 
 function hasValue(input) {
-  if (input === '') {
+  if (input === "") {
     return false;
   }
   return true;
 }
 
-form.addEventListener('click', (event) => {
+form.addEventListener("click", (event) => {
   const inputs = document.querySelectorAll("#bookForm input[type='text']");
   const titleValue = inputs[0].value;
   const authorValue = inputs[1].value;
@@ -68,7 +101,7 @@ form.addEventListener('click', (event) => {
 });
 
 function CheckLocalInput() {
-  const data = JSON.parse(localStorage.getItem('data'));
+  const data = JSON.parse(localStorage.getItem("data"));
   if (data !== null) {
     books.initializeBooks(data);
     for (let i = 0; i < data.length; i += 1) {
