bd5f2ac1f6790d016ef9af050cc99ae227a6b3fe
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-08-09 19:49:30 -0500

    Add fix for local storage & fix for linters

--- js/main.js
+++ js/main.js
@@ -1,48 +1,12 @@
 let booksArray = [];
-let Book = function (title, author) {
+const Book = function (title, author) {
   this.title = title;
   this.author = author;
 };
-let booksDisplaySection = document.querySelector("#booksDisplay");
-let form = document.querySelector("#submitBttn");
+const booksDisplaySection = document.querySelector('#booksDisplay');
+const form = document.querySelector('#submitBttn');
+let ids = 0;
 
-form.addEventListener("click", (event) => {
-  let inputs = document.querySelectorAll("#bookForm input[type='text']");
-  let titleValue = inputs[0].value;
-  let authorValue = inputs[1].value;
-  createBook(titleValue, authorValue);
-  CheckInput();
-});
-
-let createBook = function (titleValue, authorValue) {
-  let newBook = new Book(titleValue, authorValue);
-  booksArray.push(newBook);
-  insertBookStructure(newBook);
-}
-
-let insertBookStructure = function(book) {
-  let divSection = document.createElement("div");
-  divSection.innerHTML =
-    `<p>${book.title}</p>` +
-    `<p>${book.author}</p>` +
-    `<button class = 'removeButton'>Remove</button><br>`;
-  divSection.id = `${booksArray.length - 1}`;
-
-  booksDisplaySection.appendChild(divSection);
-  removeBook(divSection);
-}
-
-let removeBook = function (div) {
-  let removeBttn = document.getElementById(div.id).querySelector(".removeButton");
-  removeBttn.addEventListener("click", function () {
-    booksArray.splice(div.id, 1);
-    div.remove();
-    // if(booksArray.length > 0)
-    // CheckInput();
-  });
-}
-
-// Storage
 function localStorageAv() {
   const test = 'test';
   try {
@@ -67,24 +31,51 @@ const CheckInput = () => { // eslint-disable-line no-unused-vars
   }
 };
 
-let initializeBooks = function(data) {
-  if(data.length > 0){
+const removeBook = function (div) {
+  const removeBttn = document.getElementById(div.id).querySelector('.removeButton');
+  removeBttn.addEventListener('click', () => {
+    booksArray.splice(div.id, 1);
+    CheckInput();
+    div.remove();
+  });
+};
+
+const insertBookStructure = function (book, initialDisplay) {
+  const divSection = document.createElement('div');
+  divSection.innerHTML = `<p>${book.title}</p>`
+    + `<p>${book.author}</p>`
+    + '<button class = \'removeButton\'>Remove</button><br>';
+  if (!initialDisplay) {
+    divSection.id = `${booksArray.length - 1}`;
+  } else {
+    divSection.id = `${ids}`;
+    ids += 1;
+  }
+
+  booksDisplaySection.appendChild(divSection);
+  removeBook(divSection);
+};
+
+const createBook = function (titleValue, authorValue) {
+  const newBook = new Book(titleValue, authorValue);
+  booksArray.push(newBook);
+  insertBookStructure(newBook, false);
+};
+
+form.addEventListener('click', (event) => {
+  const inputs = document.querySelectorAll("#bookForm input[type='text']");
+  const titleValue = inputs[0].value;
+  const authorValue = inputs[1].value;
+  createBook(titleValue, authorValue);
+  CheckInput();
+  event.preventDefault();
+});
+
+const initializeBooks = function (data) {
+  if (data.length > 0) {
     booksArray = data;
   }
-}
-
-function CheckLocalInput() {
-  const data = JSON.parse(localStorage.getItem('books'));
-  if (data !== null) {
-    initializeBooks(data);
-    for (let i = 0; i < data.length; i += 1) {
-      if (hasValue(data[i])) {
-        insertBookStructure(data[i]);
-      }
-    }
-  }
-}
-CheckLocalInput();
+};
 
 function hasValue(input) {
   if (input === '') {
@@ -92,3 +83,18 @@ function hasValue(input) {
   }
   return true;
 }
+
+function CheckLocalInput() {
+  const data = JSON.parse(localStorage.getItem('books'));
+  if (data !== null) {
+    initializeBooks(data);
+    ids = 0;
+    for (let i = 0; i < data.length; i += 1) {
+      if (hasValue(data[i])) {
+        insertBookStructure(data[i], true);
+      }
+    }
+  }
+}
+
+CheckLocalInput();
